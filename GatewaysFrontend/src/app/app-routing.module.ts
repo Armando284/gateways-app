f351f8e681f1232a6f2c4227d84b43f675eb5737
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-20 12:07:55 -0400

    #30 Adding routes

--- GatewaysFrontend/src/app/app-routing.module.ts
+++ GatewaysFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GatewaysListComponent } from './components/gateways-list/gateways-list.component';
+import { GatewaysDetailsComponent } from './components/gateways-details/gateways-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GatewaysListComponent },
+  { path: 'gateway', component: GatewaysDetailsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
