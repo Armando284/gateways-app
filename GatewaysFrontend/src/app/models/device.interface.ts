c8901e9d9b594d19d6e45a18255fd8993e6b06d0
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-20 12:05:38 -0400

    #27 Adding id parameter to models

--- GatewaysFrontend/src/app/models/device.interface.ts
+++ GatewaysFrontend/src/app/models/device.interface.ts
@@ -1,4 +1,5 @@
 export interface Device {
+  _id?: string, // This is optional since it is created by mongodb and it's not available at creation time.
   UID: Number,
   vendor: string,
   createdAt: Date,
--- GatewaysFrontend/src/app/models/gateway.interface.ts
+++ GatewaysFrontend/src/app/models/gateway.interface.ts
@@ -1,6 +1,7 @@
 import { Device } from './device.interface';
 
 export interface Gateway {
+  _id?: string, // This is optional since it is created by mongodb and it's not available at creation time.
   serialNumber: string,
   name: string,
   ipv4: string,
