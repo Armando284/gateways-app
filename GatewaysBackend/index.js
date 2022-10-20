a16127d88718debeca93c1715bd95270a38ec0d3
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-20 09:59:40 -0400

    #17 Adding cors permissions and body parsers

--- GatewaysBackend/index.js
+++ GatewaysBackend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-// const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +9,16 @@ const api = require('./routes/api');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Cross Origin middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.listen(PORT, () => {
   console.log('App listening on port:', PORT);
 });
