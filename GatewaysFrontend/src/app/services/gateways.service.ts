686d7e02d77c50b753b8884b5bc2145ee637f86f
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-20 12:06:48 -0400

    #28 Finishing all service routes

--- GatewaysFrontend/src/app/services/gateways.service.ts
+++ GatewaysFrontend/src/app/services/gateways.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Gateway, Device } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GatewaysService {
 
-  constructor() { }
+  API: string = environment.api;
+  gateways: Gateway[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  AddGateway(gateway: Gateway): Observable<Gateway> {
+    return this.http.post<Gateway>(`${this.API}/gateways`, gateway);
+  }
+
+  GetAllGateways(): Observable<Gateway[]> {
+    return this.http.get<Gateway[]>(`${this.API}/gateways`);
+  }
+
+  GetGateway(gatewayId: string): Observable<Gateway> {
+    return this.http.get<Gateway>(`${this.API}/gateways/${gatewayId}`);
+  }
+
+  UpdateGateway(gateway: Gateway): Observable<Gateway> {
+    return this.http.put<Gateway>(`${this.API}/gateways/${gateway._id}`, gateway)
+  }
+
+  DeleteGateway(gatewayId: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.API}/gateways/${gatewayId}`);
+  }
+
 }
