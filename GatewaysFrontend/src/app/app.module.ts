934a3c8694bf23752c68e2c3bf82b6eacc6332ee
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-23 20:36:27 -0400

    #38 Adding new components to app module

--- GatewaysFrontend/src/app/app.module.ts
+++ GatewaysFrontend/src/app/app.module.ts
@@ -5,14 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { GatewaysListComponent } from './components/gateways-list/gateways-list.component';
 import { GatewaysDetailsComponent } from './components/gateways-details/gateways-details.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AlertComponent } from './shared/alert/alert.component';
 
 import { GatewaysService } from './services/gateways.service';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SearchService } from './services/search.service';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -21,16 +27,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     GatewaysDetailsComponent,
     NavbarComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [
     GatewaysService,
+    SearchService,
   ],
   bootstrap: [AppComponent]
 })
