db8896b1f846a2c084b50347fd369acba32fa975
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-10-23 20:38:03 -0400

    #40 Adding unit tests for the navbar component

--- GatewaysFrontend/src/app/shared/navbar/navbar.component.spec.ts
+++ GatewaysFrontend/src/app/shared/navbar/navbar.component.spec.ts
@@ -1,25 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [NavbarComponent],
+      imports: [RouterTestingModule.withRoutes([])],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
+    // Todo: inject the Router and RouterTestingModule and configure so this works
     expect(component).toBeTruthy();
   });
 });
